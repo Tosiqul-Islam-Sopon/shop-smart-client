a87a20fe168471c25730f589c8059ec22a1ee7ff
Tosiqul Islam <sopon.tosiq@gmail.com>
2024-08-16 22:39:20 +0600

    categories and brands are fetched from database

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -12,19 +12,35 @@ const Products = () => {
     const [sortOption, setSortOption] = useState('');
     const axiosBase = useAxiosBase();
 
-    const { data, isLoading, isError } = useQuery({
+    const { data: productData, isLoading: isProductsLoading, isError: isProductsError } = useQuery({
         queryKey: ['products', currentPage, searchQuery, selectedBrand, selectedCategory, priceRange, sortOption],
         queryFn: async () => {
             const params = new URLSearchParams({
                 page: currentPage,
                 limit: 15,
-                // search: searchQuery,
-                // brand: selectedBrand,
-                // category: selectedCategory,
-                // priceRange,
-                // sort: sortOption
+                search: searchQuery,
+                brand: selectedBrand,
+                category: selectedCategory,
+                priceRange,
+                sort: sortOption
             }).toString();
             const response = await axiosBase.get(`/products?${params}`);
+            return response.data;
+        }
+    });
+
+    const { data: categories, isLoading: isCategoriesLoading, isError: isCategoriesError } = useQuery({
+        queryKey: ['categories'],
+        queryFn: async () => {
+            const response = await axiosBase.get('/categories');
+            return response.data;
+        }
+    });
+
+    const { data: brands, isLoading: isBrandsLoading, isError: isBrandsError } = useQuery({
+        queryKey: ['brands'],
+        queryFn: async () => {
+            const response = await axiosBase.get('/brands');
             return response.data;
         }
     });
@@ -36,7 +52,7 @@ const Products = () => {
     };
 
     const handleNext = () => {
-        if (data && currentPage < data.pagination.totalPages) {
+        if (productData && currentPage < productData.pagination.totalPages) {
             setCurrentPage(currentPage + 1);
         }
     };
@@ -49,35 +65,42 @@ const Products = () => {
         e.preventDefault();
         const searchValue = e.target.search.value;
         setSearchQuery(searchValue);
-        setCurrentPage(1); 
+        setCurrentPage(1);
     };
 
     const handleBrandChange = (e) => {
         setSelectedBrand(e.target.value);
-        setCurrentPage(1); // Reset to the first page on filter
+        setCurrentPage(1);
     };
 
     const handleCategoryChange = (e) => {
         setSelectedCategory(e.target.value);
-        setCurrentPage(1); // Reset to the first page on filter
+        setCurrentPage(1);
     };
 
     const handlePriceRangeChange = (e) => {
         setPriceRange(e.target.value);
-        setCurrentPage(1); // Reset to the first page on filter
+        setCurrentPage(1);
     };
 
     const handleSortChange = (e) => {
         setSortOption(e.target.value);
-        setCurrentPage(1); // Reset to the first page on sort
+        setCurrentPage(1);
     };
 
-    if (isLoading) {
-        return <p>Loading...</p>;
+    if (isProductsLoading || isCategoriesLoading || isBrandsLoading) {
+        return (
+            <div className='mx-auto w-fit'>
+                <span className="loading loading-ring loading-xs"></span>
+                <span className="loading loading-ring loading-sm"></span>
+                <span className="loading loading-ring loading-md"></span>
+                <span className="loading loading-ring loading-lg"></span>
+            </div>
+        );
     }
 
-    if (isError) {
-        return <p>Error loading products.</p>;
+    if (isProductsError || isCategoriesError || isBrandsError) {
+        return <p>Error loading data.</p>;
     }
 
     return (
@@ -106,33 +129,15 @@ const Products = () => {
             <div className="flex flex-col md:flex-row justify-between items-center mx-6 my-4 gap-4">
                 <select value={selectedBrand} onChange={handleBrandChange} className="px-4 py-2 border rounded-lg w-full md:w-auto">
                     <option value="">All Brands</option>
-                    <option value="TechPro">TechPro</option>
-                    <option value="SoundMaster">SoundMaster</option>
-                    <option value="PowerGear">PowerGear</option>
-                    <option value="FitTrack">FitTrack</option>
-                    <option value="HomeSmart">HomeSmart</option>
-                    <option value="EcoStyle">EcoStyle</option>
-                    <option value="HealthCare">HealthCare</option>
-                    <option value="ComfortHome">ComfortHome</option>
-                    <option value="KitchEase">KitchEase</option>
-                    <option value="CleanSweep">CleanSweep</option>
-                    <option value="ChillZone">ChillZone</option>
-                    <option value="NatureAir">NatureAir</option>
+                    {brands.map((brand) => (
+                        <option key={brand} value={brand}>{brand}</option>
+                    ))}
                 </select>
                 <select value={selectedCategory} onChange={handleCategoryChange} className="px-4 py-2 border rounded-lg w-full md:w-auto">
                     <option value="">All Categories</option>
-                    <option value="Electronics">Electronics</option>
-                    <option value="Wearables">Wearables</option>
-                    <option value="Footwear">Footwear</option>
-                    <option value="Health & Beauty">Health & Beauty</option>
-                    <option value="Fitness">Fitness</option>
-                    <option value="Home & Office">Home & Office</option>
-                    <option value="Bags & Luggage">Bags & Luggage</option>
-                    <option value="Accessories">Accessories</option>
-                    <option value="Kitchen Appliances">Kitchen Appliances</option>
-                    <option value="Home Appliances">Home Appliances</option>
-                    <option value="Home & Living">Home & Living</option>
-                    <option value="Outdoor & Garden">Outdoor & Garden</option>
+                    {categories.map((category) => (
+                        <option key={category} value={category}>{category}</option>
+                    ))}
                 </select>
                 <select value={priceRange} onChange={handlePriceRangeChange} className="px-4 py-2 border rounded-lg w-full md:w-auto">
                     <option value="">All Price Ranges</option>
@@ -150,44 +155,57 @@ const Products = () => {
                 </select>
             </div>
 
+            {searchQuery && (
+                <p className="mx-6 my-4 text-lg font-semibold text-gray-700 text-center">
+                    Showing results for {searchQuery}
+                </p>
+            )}
 
-            {/* Product Grid */}
-            <div className="grid grid-cols-3 gap-6 mx-6 my-10">
-                {data.products.map((product) => (
-                    <ProductCard product={product} key={product._id} />
-                ))}
-            </div>
+            {/* Product Grid or No Products Found Message */}
+            {productData.products.length > 0 ? (
+                <div className="grid grid-cols-3 gap-6 mx-6 my-10">
+                    {productData.products.map((product) => (
+                        <ProductCard product={product} key={product._id} />
+                    ))}
+                </div>
+            ) : (
+                <div className="text-center text-2xl font-semibold text-red-500 my-10">
+                    No products found. Try adjusting your search or filter criteria.
+                </div>
+            )}
 
             {/* Pagination */}
-            <div className="flex flex-col items-center mx-4 my-6">
-                <div className="flex justify-between items-center w-full mb-4">
-                    <button
-                        className="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400"
-                        onClick={handlePrevious}
-                        disabled={currentPage === 1}
-                    >
-                        Previous
-                    </button>
-                    <div className="flex flex-wrap gap-2">
-                        {[...Array(data.pagination.totalPages)].map((_, index) => (
-                            <button
-                                key={index + 1}
-                                className={`px-3 py-1 rounded-lg ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black hover:bg-gray-300'}`}
-                                onClick={() => handlePageChange(index + 1)}
-                            >
-                                {index + 1}
-                            </button>
-                        ))}
+            {productData.products.length > 0 && (
+                <div className="flex flex-col items-center mx-4 my-6">
+                    <div className="flex justify-between items-center w-full mb-4">
+                        <button
+                            className="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400"
+                            onClick={handlePrevious}
+                            disabled={currentPage === 1}
+                        >
+                            Previous
+                        </button>
+                        <div className="flex flex-wrap gap-2">
+                            {[...Array(productData.pagination.totalPages)].map((_, index) => (
+                                <button
+                                    key={index + 1}
+                                    className={`px-3 py-1 rounded-lg ${currentPage === index + 1 ? 'bg-blue-500 text-white' : 'bg-gray-200 text-black hover:bg-gray-300'}`}
+                                    onClick={() => handlePageChange(index + 1)}
+                                >
+                                    {index + 1}
+                                </button>
+                            ))}
+                        </div>
+                        <button
+                            className="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400"
+                            onClick={handleNext}
+                            disabled={currentPage === productData.pagination.totalPages}
+                        >
+                            Next
+                        </button>
                     </div>
-                    <button
-                        className="px-4 py-2 bg-gray-300 text-black rounded-lg hover:bg-gray-400"
-                        onClick={handleNext}
-                        disabled={currentPage === data.pagination.totalPages}
-                    >
-                        Next
-                    </button>
                 </div>
-            </div>
+            )}
         </div>
     );
 };
